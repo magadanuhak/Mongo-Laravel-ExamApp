52df48af49160fe3444db999f68ea6c02eefb128
daniel <magadanuahak@gmail.com>
2022-06-01 09:00:58 +0300

    Tasks CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,10 +10,4 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 class UserController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function create()
-    {
-        Task::query()->create(['title' => 'examen mongo', 'description' => 'mongodb in php '])->toArray();
-        dd(Task::all());
-    }
 }
